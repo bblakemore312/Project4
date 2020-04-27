83e5253cddd0a269a24ad3ff9ed2c6814a79a367
bonfirepilot <60601856+bonfirepilot@users.noreply.github.com>
2020-04-27 14:03:24 -0500

    Update Click-CLI.py

--- Click-CLI.py
+++ Click-CLI.py
@@ -2,7 +2,7 @@ import API
 import os
 import click
 
-@click.command()
+@cli.command()
 @click.option("--Factorial", prompt="The number you want to factor:", help="The number you want factored.")
 @click.option("--Fibonacci", prompt="How many numbers you want Fibonacci'd", help="The amount of places to see in the Fibonacci Sequence.")
 @click.option("--Prime", prompt="The number to test", help="The number we want to check if is prime.")
@@ -10,7 +10,7 @@ import click
 @click.option("--keyRecord", prompt="Your desired key to input", help="The key to add.")
 @click.option("--keyRetrieve", prompt="Your desired key to retrieve", help="The key to find.")
 
-def rootTest(funcTest):
+def rootTest():
   root()
 
 def factorialTest(Factorial):
